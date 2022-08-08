67b3ddb698e683635b412de82b9a61fa2b4edb1c
Ross-Morgan <morgan.ross.512@gmail.com>
2022-08-08 19:23:23 +0100

    allow negative factor pairs

--- src/pyfuck2/factors.py
+++ src/pyfuck2/factors.py
@@ -15,15 +15,45 @@ def closest_square(n: int) -> int:
 
 
 def smallest_sum_factor_pair(n: int) -> tuple[int, int]:
+    n = abs(n)
     pairs = [(i, int(n / i)) for i in range(1, int(n ** 0.5)+1) if n % i == 0]
     pair_sums = list(map(sum, pairs))
 
     return pairs[pair_sums.index(min(pair_sums))]
 
 
-def nice_factor_pair(n: int) -> tuple[tuple[int, int], int]:
+def smallest_sum_factors_and_remainder(n: int) -> tuple[tuple[int, int], int]:
+    original = n
+    n = abs(n)
+
     if n % 1:
         raise ValueError("n must be an integer")
 
-    while True:
-        pass
+    factors: list[int] = []
+    origins: list[tuple[int, int]] = []
+
+    # Number of iterations to check
+    runs = 5
+
+    # Multiples to check for factors
+    k = 4
+    a = -k
+
+    # Round down to nearest multiple of k
+    n -= n % k
+    a += n % k
+
+    n += k
+
+    for _ in range(runs):
+        if n < k:
+            break
+
+        n -= k
+        a += k
+        factors.append(sum(smallest_sum_factor_pair(n)) + (original - n))
+        origins.append((n, (original - n)))
+
+    if not factors:
+        return (0, 0), original
+    return smallest_sum_factor_pair(origins[factors.index(min(factors))][0]), origins[factors.index(min(factors))][1]
